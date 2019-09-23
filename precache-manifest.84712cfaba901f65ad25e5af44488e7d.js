8bd73445b4c87927a1a017e171bd8ea9e735817e
Andrej Oravkin <aoravkin@gmail.com>
2019-09-23 22:58:16 +0100

    Updates

---
+++ precache-manifest.84712cfaba901f65ad25e5af44488e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7ef91de4281e68d1e89a0ffa55f5c6",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "f1c4e96345261cb53825",
+    "url": "/Monsters-Rolodex/static/css/main.b73f95d3.chunk.css"
+  },
+  {
+    "revision": "24108f942f6ec7ca93a1",
+    "url": "/Monsters-Rolodex/static/js/2.bffc69f7.chunk.js"
+  },
+  {
+    "revision": "f1c4e96345261cb53825",
+    "url": "/Monsters-Rolodex/static/js/main.00e59277.chunk.js"
+  },
+  {
+    "revision": "361eaa888cdc48caf9bb",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.e8625e9b.js"
+  }
+]);
